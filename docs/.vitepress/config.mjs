8626eb33f5490514e3754d13fed221c70155bf2b
fruge <64948856+amoureux555@users.noreply.github.com>
2025-02-21 17:31:45 +0800

    修改base

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "WuUI",
   description: "一个基于Element-ui进行二次封装的UI组件库",
-  base: process.env.NODE_ENV === 'production' ? '/docs/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/docs/' : '/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
